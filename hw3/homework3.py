fec90a3cc2eecf175361e02cf40aaea9e2f83488
Eric <hchsieh0811@gmail.com>
2017-11-11 20:46:19 -0800

    hierarchy class construction

--- hw3/homework3.py
+++ hw3/homework3.py
@@ -2,34 +2,129 @@
 # for loop read file:
 # for line in open(input_file_name):
 #     print(line)
+import pdb
 
-def Parser(sentence):
-    print('parser')
-    for index in range(0,len(sentence)):
+class KB(object):
+    """docstring for KB"""
+    def __init__(self):
+        super(KB, self).__init__()
+        self.sentence = []
+
+class Sentence(object):
+    """docstring for Sentence"""
+    def __init__(self):
+        super(Sentence, self).__init__()
+        self.predicate = []
+        
+class Predicate:
+    def __init__(self,name):
+        super(Predicate, self).__init__()
+        self.name = name
+        self.argument = []
+class Argument:
+    def __init__(self, name, attribute):
+        super(Argument, self).__init__()
+        self.name = name
+        self.attribute = attribute
+
+def Parser(sBuffer):
+    print('parser:')
+
+    # Initialize myKB:
+
+    myKB = KB()
+
+    # Sentence index
+    sIndex = 0
+
+    # Predicate index
+    pIndex = 0
+
+    mySentence = Sentence()
+    myKB.sentence.append(mySentence)
+
+    myPredicate = Predicate('')
+    myKB.sentence[sIndex].predicate.append(myPredicate)
+
+    myArgument = Argument('','')
+    myKB.sentence[sIndex].predicate[pIndex].argument.append(myArgument)
+    exit()
+    stack = []
+    
+
+    # Check if parsing inside the query or not
+    inBracket = False;
+
+    for index in range(0,len(sBuffer)):
+        # Not: push "~" into the sentence as part of the PREDICATE
+        if(sBuffer[index] == '~'):
+            # pdb.set_trace()
+            stack += sBuffer[index]
+            #stack += sentence[index]
+
         # Upper case letter
-        if(sentence[index].isupper() and sentence[index].isalpha()):
-            print('{} is upper'.format(sentence[index]))
+        # Push letter into stack
+        elif(sBuffer[index].isupper() and sBuffer[index].isalpha()):
+                stack += sBuffer[index]
+
         # Lower case letter
-        elif(sentence[index].islower() and sentence[index].isalpha()):
-            print('{} is not upper'.format(sentence[index]))
-        # Left Parenthesis
-        elif(sentence[index] == '('):
-            print('(')
-        # Right Parenthesis
-        elif(sentence[index] == ')'):
-            print(')')
-        # Comma
-        elif(sentence[index] == ','):
-            print(',')
-        # OR
-        elif(sentence[index] == '|'):
-            print('or')
-        # Not
-        elif(sentence[index] == '~'):
-            print('not')
+        # Push letter into stack
+        elif(sBuffer[index].islower() and sBuffer[index].isalpha()):
+                stack += sBuffer[index]
+
+        # Left Parenthesis, pop content in the stack out as PREDICATE
+        # things after Left Praenthesis should be CONSTANT or VARIABLE
+        elif(sBuffer[index] == '('):
+            inBracket = True
+            tempstack = []
+            tempstack += stack
+            tempPredicate = Predicate(tempstack)
+            print(myKB.sentence[sIndex].predicate[pIndex])
+            exit()
+            stack = []
+
+        # Comma: separate the CONSTANT or VARIABLES
+        elif(sBuffer[index] == ','):
+            if(stack[0].islower()):
+                tempStack = []
+                tempStack += stack
+                myArgument = Argument(tempStack,'CONSTANT')
+                print('myArgument = ', myArgument.name,'attribute =',myArgument.attribute)
+                stack = []
+            elif(stack[0].isupper()):
+                tempStack = []
+                tempStack += stack
+                myArgument = Argument(tempStack,'VARIABLE')
+                print('myArgument = ', myArgument.name,'attribute =',myArgument.attribute)
+                stack = []
+
+        # Right Parenthesis: ending of predicate
+        elif(sBuffer[index] == ')'):
+            inBracket = False
+            if(stack[0].islower()):
+                # Insert argument into predicate
+                tempStack = []
+                tempStack += stack
+                myArgument = Argument(tempStack,'CONSTANT')
+
+
+                stack = []
+            elif(stack[0].isupper()):
+                tempStack = []
+                tempStack += stack
+                myArgument = Argument(tempStack,'VARIABLE')
+                print('myArgument = ', myArgument.name,'attribute =',myArgument.attribute)
+                stack = []
+        
+        # OR: add another predicate into the sentence
+        elif(sBuffer[index] == '|'):
+            pIndex += 1
+            print('add predicate, current predicate number = {0}'.format(pIndex))
+
         # EOL
-        elif(sentence[index] == '\n'):
-            print('EOL')
+        elif(sBuffer[index] == '\n'):
+            sIndex += 1
+            print('add one sentence, current sentence number = {0}'.format(sIndex))
         else:
             print('*******ELSE**********')
 
@@ -46,28 +141,26 @@ if __name__ == '__main__':
             read_data = line.strip().replace(' ', '')
             num_of_query = int(read_data)
             query = []
-            sentence = []
+            sBuffer = []
             for i in range(0,num_of_query):
                 query += input_f.readline().replace(' ', '')
                 # print(query)
             num_of_sentence = int(input_f.readline())
             for i in range(0,num_of_sentence):
                 # sentence.append(input_f.readline())
-                sentence += input_f.readline().replace(' ', '')
+                sBuffer += input_f.readline().replace(' ', '')
     input_f.closed
-
+    sentence = [123]
     # print(query[0])
     # print(sentence)
     # print(len(sentence))
 
-    stack = []
     # stack += sentence
     # print(len(sentence))
     # a = len(sentence)
     
         # print('sentence {0} = '.format(index),sentence[index])
         # print('stack = ', stack)
-    Parser(sentence)
-    print('1234')
+    Parser(sBuffer)
 
 
