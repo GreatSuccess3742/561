18ff2ada486d6047f41cd995f104b49de5da5def
GreatSuccess3742 <erichsieh@Ericde-MacBook-Pro.local>
2017-11-15 23:32:34 -0800

    add some print function, and Standardize variables

--- hw3/homework3.py
+++ hw3/homework3.py
@@ -35,6 +35,48 @@ def FindPredicateArgument(predicate):
     pArgument = tempArgument.split(',')
     return pArgument
 
+def PrintKB(sentence):
+    for i in range(0,len(sentence)):
+        print('--------- sentence {0} = '.format(i), '---------')
+        for j in range(0, len(sentence[i])):
+            print('Negate = ', sentence[i][j].negate)
+            print('Predicate {0} = '.format(j) ,sentence[i][j].name, '\n contains:')
+            for k in range(0, len(sentence[i][j].argument)):
+                print('arg {0} = '.format(k), sentence[i][j].argument[k])
+
+def StandardizeVariable(sentence):
+    # Standardize by adding number of the setence in the back of the variable
+    for i in range(0,len(sentence)):
+        for j in range(0, len(sentence[i])):
+            for k in range(0, len(sentence[i][j].argument)):
+                if(sentence[i][j].argument[k][0].islower()):
+                    sentence[i][j].argument[k] += str(i)
+
+def Resolve(s1,s2):
+    print('here = ',s1[1].name)
+    print('here = ',s2[0].name)
+
+def Resolution(singleQuery,sentence):
+    negateQuery = Predicate(singleQuery.negate, singleQuery.name, singleQuery.argument)
+    tempSentence = sentence[:]
+
+    # Negate the Query and put it into KB:
+    negateQuery.negate = not negateQuery.negate
+    tempPredicate = []
+    dummy = []
+    tempPredicate = Predicate(negateQuery.negate, negateQuery.name, negateQuery.argument)
+    dummy.append(tempPredicate)
+    tempSentence.append(dummy)
+
+    new = []
+    while(1):
+        for i in range(0, len(tempSentence)):
+            for j in range(i+1, len(tempSentence)): #TODO
+
+                Resolve(tempSentence[i],tempSentence[j])
+                exit()
+
+
 
 # Main fucntion:
 if __name__ == '__main__':
@@ -60,8 +102,6 @@ if __name__ == '__main__':
     input_f.closed
 
     # Organizing Query
-    print('len(tempQuery) = ', len(tempQuery))
-    print('tempQuery = ',tempQuery)
     query = []
     for i in range(0, len(tempQuery)):
         if(tempQuery[i][0] == '~'):
@@ -72,8 +112,6 @@ if __name__ == '__main__':
         qArgument = FindPredicateArgument(tempQuery[i])
         myQuery = Predicate(qNegate, qName, qArgument)
         query.append(myQuery)
-    x = 1
-    print('query = ',query[x].negate , query[x].name, query[x].argument)
 
     # Turn the tempSentence into sentence, sentence[i] is a list of predicate
     sentence = []
@@ -94,8 +132,13 @@ if __name__ == '__main__':
         sentence.append(predicate[:])
         predicate = []
 
+    # Standardize Variables:
+    StandardizeVariable(sentence)
 
     # Do resolution:
+    for qIndex in range(0,len(query)):
+        Resolution(query[qIndex],sentence)
+
 
 
 
